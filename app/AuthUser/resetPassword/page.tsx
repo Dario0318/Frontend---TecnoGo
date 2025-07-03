a68ac3d50a251749390d277a4f91646b524b033d
Dario0318 <drfq180305@gmail.com>
2025-07-03 16:26:46 -0500

    ultima correccion

--- app/AuthUser/resetPassword/page.tsx
+++ app/AuthUser/resetPassword/page.tsx
@@ -1,10 +1,11 @@
+"use client"
+
+import { useSearchParams } from "next/navigation"
 import { ResetPasswordForm } from "./components/resetPasswordForm"
 
-type Props = {
-  searchParams?: { code?: string }
-}
+export default function ResetPasswordPage() {
+  const searchParams = useSearchParams()
+  const code = searchParams.get("code") ?? ""
 
-export default function ResetPasswordPage({ searchParams }: Props) {
-  const code = searchParams?.code ?? ""
   return <ResetPasswordForm code={code} />
 }
