bd727b91715640c154ff32e219ba252913538a56
Dario0318 <drfq180305@gmail.com>
2025-07-03 15:57:05 -0500

    nuevoss

--- app/AuthUser/resetPassword/page.tsx
+++ app/AuthUser/resetPassword/page.tsx
@@ -1,8 +1,9 @@
 import { ResetPasswordForm } from "./components/resetPasswordForm"
 
-// 👇 Este es el tipo correcto según Next.js 14+
 type Props = {
-  searchParams: { code?: string }
+  searchParams: {
+    code?: string
+  }
 }
 
 export default function ResetPasswordPage({ searchParams }: Props) {
