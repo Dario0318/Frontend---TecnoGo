dc4a4cb276bba51c8b0891f76df77fb513b56fe6
Dario0318 <drfq180305@gmail.com>
2025-07-03 13:33:43 -0500

    nueva configuracion en next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,28 @@
+import type { NextConfig } from 'next';
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
-        port: '1337', // asegúrate de poner el puerto que usas
+        port: '1337',
         pathname: '/uploads/**',
       },
-        {
+      {
         protocol: 'https',
         hostname: 'backend-tecnogo.onrender.com',
-        port: '443', // asegúrate de poner el puerto que usas
-        pathname: '/',
+        port: '', // El puerto 443 es implícito en https, así que puede ir vacío
+        pathname: '/uploads/**', // Especifica bien la ruta si es para imágenes
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
